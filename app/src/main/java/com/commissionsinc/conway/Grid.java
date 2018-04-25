8f0f74306f86248c541975365951df28e5f18664
Michael Lese <michael.lese08@gmail.com>
2018-04-25 18:42:36 -0400

    needs implementation

--- app/src/main/java/com/commissionsinc/conway/Grid.java
+++ app/src/main/java/com/commissionsinc/conway/Grid.java
@@ -32,14 +32,6 @@ public class Grid {
         }
     }
 
-    private void initEmpty(Cell newMatrix[][]) {
-        for (int x = 0; x < numcols; x++) {
-            for (int y = 0; y < numrows; y++) {
-                newMatrix[x][y] = new Cell(false);
-            }
-        }
-    }
-
     public void draw(Canvas canvas, Paint paint) {
         canvas.drawColor(Color.BLACK);
         paint.setColor(Color.GREEN);
@@ -59,79 +51,6 @@ public class Grid {
     4. Any dead cell with exactly 3 live neighbors becomes a live cell
      */
     public void update() {
-        Cell newMatrix[][] = new Cell[numcols][numrows];
-        initEmpty(newMatrix);
-        ArrayList<Cell> neighbors;
-        for (int x = 0; x < numcols; x++) {
-            for (int y = 0; y < numrows; y++) {
-                neighbors = getNeighbors(x, y);
-                int numNeighbors = countNeighbors(neighbors);
 
-                if (cellMatrix[x][y].isAlive()) {
-                    if (numNeighbors < 2) newMatrix[x][y].setAlive(false);
-                    if (numNeighbors == 2 || numNeighbors == 3) newMatrix[x][y].setAlive(true);
-                    if (numNeighbors > 3) newMatrix[x][y].setAlive(false);
-                } else {
-                    if (numNeighbors == 3) {
-                        newMatrix[x][y].setAlive(true);
-                    }
-                }
-            }
-        }
-        cellMatrix = newMatrix;
-    }
-
-    private ArrayList<Cell> getNeighbors(int x, int y) {
-        ArrayList<Cell> neighbors = new ArrayList<>(8);
-
-        // Left
-        if (x - 1 >= 0) {
-            neighbors.add(cellMatrix[x-1][y]);
-
-            // Upper Left
-            if (y - 1 >= 0) {
-                neighbors.add(cellMatrix[x-1][y-1]);
-            }
-
-            // Bottom Left
-            if (y + 1 < numrows) {
-                neighbors.add(cellMatrix[x-1][y+1]);
-            }
-        }
-
-        // Right
-        if (x + 1 < numcols) {
-            neighbors.add(cellMatrix[x+1][y]);
-
-            // Upper Right
-            if (y - 1 >= 0) {
-                neighbors.add(cellMatrix[x+1][y-1]);
-            }
-
-            // Bottom Right
-            if (y + 1 < numrows) {
-                neighbors.add(cellMatrix[x+1][y+1]);
-            }
-        }
-
-        // Top
-        if (y - 1 >= 0) {
-            neighbors.add(cellMatrix[x][y-1]);
-        }
-
-        // Bottom
-        if (y + 1 < numrows) {
-            neighbors.add(cellMatrix[x][y+1]);
-        }
-
-        return neighbors;
-    }
-
-    private int countNeighbors(ArrayList<Cell> neighbors) {
-        int aliveNeighbors = 0;
-        for (Cell cell : neighbors) {
-            if (cell.isAlive()) aliveNeighbors++;
-        }
-        return aliveNeighbors;
     }
 }
